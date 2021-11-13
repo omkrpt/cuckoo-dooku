4ddfd67d328acb30df8e274658a1827cf6639729
vignespacap <vignesh.palanisamy@capillarytech.com>
2021-11-13 14:56:56 +0530

    extension dependencies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import './App.css';
-import 'antd/dist/antd.css';
-import { Button } from 'antd';
+/*global chrome*/
+import { useEffect } from "react";
+import "./App.css";
+import "antd/dist/antd.css";
+import { Button } from "antd";
 
 function App() {
+  useEffect(() => {}, []);
+
+  const alarmName = "alarmName";
+
+  function createAlarm() {
+    chrome.alarms.create(alarmName, {
+      delayInMinutes: 0,
+      periodInMinutes: 0.1,
+    });
+  }
+
+  function cancelAlarm() {
+    chrome.alarms.clear(alarmName);
+  }
+
   return (
     <div className="App">
       <h1>welcome to Count-Cuckoo</h1>
-      <Button type="primary">Primary</Button>
+      <Button type="primary" onClick={createAlarm}>
+        Start
+      </Button>
+      <Button type="primary" onClick={cancelAlarm}>
+        Clear
+      </Button>
     </div>
   );
 }
