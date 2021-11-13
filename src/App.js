db0efbc89df29ff80d0305906f7e9219703da0ae
omkar-pote <omkar.pote@capillarytech.com>
2021-11-14 03:11:32 +0530

    Altering first page layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import "antd/dist/antd.css";
 import { Row, Button } from "antd";
+import logo from './dooku_logo2.png';
 // import Input from "./components/Card/Input";
 
 import CardListing from "./components/Card/CardListing";
@@ -17,8 +18,8 @@ function App() {
 
   return (
     <div className="App">
-      <div style={{ width: "500px" }}>
-        <Row className="header">Count Cuckoo</Row>
+      <div style={{ width: "400px" }}>
+        <Row className="header"><img src={logo} alt="Logo" /></Row>
         {reminderPage.show ? (
           <AddNew id={reminderPage.id} setReminderPage={setReminderPage} />
         ) : (
