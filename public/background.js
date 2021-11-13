6cf515c5bb5ffbbf261b65ac70f4d7d122ec7657
vignespacap <vignesh.palanisamy@capillarytech.com>
2021-11-13 16:26:54 +0530

    Storage implementations commit

--- public/background.js
+++ public/background.js
@@ -1,11 +1,20 @@
 chrome.alarms.onAlarm.addListener((alarm) => {
-  console.log("alarm ring", alarm);
-  chrome.notifications.create(`test ${new Date()}`, {
-    type: "basic",
-    title: "Test Message hii",
-    message: "You are awesome!",
-    priority: 2,
-    iconUrl:
-      "https://www.google.com/images/branding/googlelogo/2x/googlelogo_light_color_272x92dp.png",
+  var alarmName = alarm.name;
+  console.warn("alarm ring", alarmName);
+  chrome.storage.sync.get(alarmName, ({ [alarmName]: msg }) => {
+    try {
+      var { title, message } = JSON.parse(msg);
+      chrome.notifications.create(`count-cuckoo-${new Date()}`, {
+        type: "basic",
+        title,
+        message,
+        priority: 2,
+        iconUrl:
+          "https://www.google.com/images/branding/googlelogo/2x/googlelogo_light_color_272x92dp.png",
+      });
+    } catch (error) {
+      console.error(error);
+      console.warn("Notification data not found for ", alarmName);
+    }
   });
 });
